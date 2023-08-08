06335d997dab8a3725e6eca5f98303a5bf366d03
Fize <chenfengzhan@qq.com>
2023-08-08 16:45:15 +0800

    打包工具。

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="fize",
+    version="0.1.0",
+    keywords=["fize"],
+    description="fize is a package for python!",
+    long_description="Afize is a package for python!",
+    license="MIT Licence",
+    url="https://github.com/fizechan/fize",
+    author="LiangjunFeng",
+    author_email="zhumavip@163.com",
+    packages=find_packages(),
+    include_package_data=True,
+    platforms="any",
+    install_requires=[]
+)
